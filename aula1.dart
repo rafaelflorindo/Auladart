0a621cdca7fd5b9ef815909ad96093e1150794bf
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2024-07-31 22:09:18 -0300

    Add files via upload

---
+++ aula1.dart
@@ -0,0 +1,40 @@
+void main() {
+  print("Ola");
+
+  int idade = 15;
+  double valor = 587.80;
+  String nome = "Rafael";
+
+  print("Nome: $nome \nIdade: $idade \nValor: $valor");
+
+  var codigo = 15;
+  var preco = 587.80;
+  String produto = "Monitor LED";
+
+  print("\n\nCódigo: $codigo \nProduto: $produto \nValor: $preco");
+
+  /*Salario*/
+  
+  double valorHora = 35.85;
+  int quantidadeHora = 40;
+  
+  double salario;
+  
+  salario = valorHora * quantidadeHora;
+  print ("Salario: R\$ $salario");
+  int descontoInss = 9;
+  double valorDesconto = (salario * descontoInss) / 100;
+  //double valorDesconto = salario * (descontoInss / 100);
+  print ("Valor Desconto $valorDesconto");
+  
+  //double salarioLiquido = salario - valorDesconto;
+  double salarioLiquido = salario - (salario * (descontoInss / 100));
+  
+  if(salarioLiquido <= 2000){
+	    double bonificacao = 500.00;
+      double salarioBonificacao = salarioLiquido + bonificacao;
+	    print ("Salário com Bonificação R\$ $salarioBonificacao");
+  }else{
+    print("Salário Liquido $salarioLiquido");
+  }
+}
