3f45924ac17f8f2dbff4dfb1f9cbf40eb569ce5d
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2024-08-07 20:19:39 -0300

    Create Aula2.dart

---
+++ Aula2.dart
@@ -0,0 +1,22 @@
+void main() {
+
+int opcao = 1;
+  
+  switch (opcao){
+      case 1: print("Cadastrar");
+      case 2: print("Listar todos");
+      case 3: print("Listar Um");
+    default: print("Opção Inválida! Tente novamente.");
+  }
+/*
+  opcao = 2;
+  
+  if(opcao==1)
+    print("Cadastrar");
+  else if(opcao==2)
+    print("Listar Todos");
+  else if(opcao==3)
+    print("Listar Um");
+  else
+    print("Opção Inválida! Tente novamente.");*/
+}
