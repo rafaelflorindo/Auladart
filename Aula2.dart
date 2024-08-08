d2e4b7200b1fe01d6fc638030860dfa6d69769d3
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2024-08-07 21:32:18 -0300

    Update Aula2.dart

--- Aula2.dart
+++ Aula2.dart
@@ -1,14 +1,14 @@
 void main() {
 
-int opcao = 1;
+int opcao = 2;
   
   switch (opcao){
-      case 1: print("Cadastrar");
-      case 2: print("Listar todos");
-      case 3: print("Listar Um");
+      case 1: print("Cadastrar Cliente");
+      case 2: print("Listar todos os Clientes");
+      case 3: print("Listar Um Cliente");
     default: print("Opção Inválida! Tente novamente.");
   }
-/*
+
   opcao = 2;
   
   if(opcao==1)
@@ -18,5 +18,8 @@ int opcao = 1;
   else if(opcao==3)
     print("Listar Um");
   else
-    print("Opção Inválida! Tente novamente.");*/
+    print("Opção Inválida! Tente novamente.");
 }
+
+
+
